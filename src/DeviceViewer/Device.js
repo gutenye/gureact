7a2b22231bddd3d255fce453a025cd0fc63ab21b
Guten Ye <ywzhaifei@gmail.com>
2016-11-28 21:01:50 +0800

    use styled-components

--- src/DeviceViewer/Device.js
+++ src/DeviceViewer/Device.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { StyleSheet, css } from 'aphrodisiac'
+import styled from 'styled-components'
 import cx from 'classnames'
 
 /*
@@ -12,119 +12,109 @@ class Device extends Component {
   render() {
     const {props: {className, device, landscape, src}} = this
     return (
-      <div className={cx(css(className), css(s.Device), device, {landscape})}>
-        <iframe className={css(s.iframe)} src={src} />
-      </div>
+      <Root className={cx(className, device, {landscape})}>
+        <Iframe src={src} />
+      </Root>
     )
   }
 }
 
-const s = StyleSheet.create({
-  Device: {
-    // position: 'relative', // tmp for DeviceViewer position: 'absolute'
-
-    '&.phone': {
-      width: 360,
-      height: 600,
-      margin: '60px 0 96px',
-    },
-
-    '&.phone.landscape': {
-      width: 600,
-      height: 360,
-      margin: 0,
-    },
-
-    '&.tablet': {
-      width: 720,
-      height: 1024,
-      margin: '60px 0 96px',
-    },
-
-    '&.tablet.landscape': {
-      width: 1024,
-      height: 720,
-      margin: 0,
-    },
-
-    '&.laptop': {
-      width: 1366,
-      height: 800,
-    },
-
-    '&.phone:before, &.tablet:before': {
-      display: 'block',
-      content: '""',
-      position: 'absolute',
-      top: 0,
-      right: 0,
-      bottom: 0,
-      left: 0,
-      margin: '-60px -16px -96px -16px',
-      'border-radius': 32,
-      background: '#666',
-    },
-
-    '&.phone.landscape:before, &.tablet.landscape:before': {
-      margin: '-16px -96px -16px -60px',
-    },
-
-    '&.phone:after, &.tablet:after': {
-      display: 'block',
-      content: '""',
-      position: 'absolute',
-      bottom: '-76px',
-      left: '50%',
-      width: '56px',
-      height: '56px',
-      'margin-left': '-28px',
-      'border-radius': '28px',
-      background: '#fff',
-    },
-
-    '&.phone.landscape:after, &.tablet.landscape:after': {
-      top: '50%',
-      right: '-76px',
-      bottom: 'auto',
-      left: 'auto',
-      'margin-top': '-28px',
-    },
-
-    '&.laptop:before': {
-      display: 'block',
-      content: '""',
-      position: 'absolute',
-      top: 0,
-      right: 0,
-      bottom: 0,
-      left: 0,
-      margin: '-24px',
-      'border-radius': '12px',
-      background: '#666',
-    },
-
-    '&.laptop:after': {
-      display: 'block',
-      content: '""',
-      position: 'absolute',
-      right: 0,
-      bottom: '-56px',
-      left: '-140px',
-      height: '56px',
-      margin: '-24px',
-      'border-bottom-right-radius': '12px',
-      'border-bottom-left-radius': '12px',
-      background: '#777',
-    },
-  },
-
-  iframe: {
-    position: 'relative',
-    width: '100%',
-    height: '100%',
-    'background-color': '#fff',
-    border: '0',
-  },
-})
+const Root = styled.div`
+  position: relative;
+  &.phone {
+    width: 360px;
+    height: 600px;
+    margin: 60px 0 96px;
+  }
+  &.phone.landscape {
+    width: 600px;
+    height: 360px;
+    margin: 0;
+  }
+  &.tablet {
+    width: 720px;
+    height: 1024px;
+    margin: 60px 0 96px;
+  }
+  &.tablet.landscape {
+    width: 1024px;
+    height: 720px;
+    margin: 0;
+  }
+  &.laptop {
+    width: 1366px;
+    height: 800px;
+  }
+  &.phone:before,
+  &.tablet:before {
+    display: block;
+    content: "";
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    margin: -60px -16px -96px -16px;
+    border-radius: 32px;
+    background: #666;
+  }
+  &.phone.landscape:before,
+  &.tablet.landscape:before {
+    margin: -16px -96px -16px -60px;
+  }
+  &.phone:after,
+  &.tablet:after {
+    display: block;
+    content: "";
+    position: absolute;
+    bottom: -76px;
+    left: 50%;
+    width: 56px;
+    height: 56px;
+    margin-left: -28px;
+    border-radius: 28px;
+    background: #fff;
+  }
+  &.phone.landscape:after,
+  &.tablet.landscape:after {
+    top: 50%;
+    right: -76px;
+    bottom: auto;
+    left: auto;
+    margin-top: -28px;
+  }
+  &.laptop:before {
+    display: block;
+    content: "";
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    margin: -24px;
+    border-radius: 12px;
+    background: #666;
+  }
+  &.laptop:after {
+    display: block;
+    content: "";
+    position: absolute;
+    right: 0;
+    bottom: -56px;
+    left: -140px;
+    height: 56px;
+    margin: -24px;
+    border-bottom-right-radius: 12px;
+    border-bottom-left-radius: 12px;
+    background: #777;
+  }
+`
+const Iframe = styled.iframe`
+  position: relative;
+  width: 100%;
+  height: 100%;
+  background-color: #fff;
+  border: 0;
+`
 
 export default Device
--- src/DeviceViewer/DeviceViewer.js
+++ src/DeviceViewer/DeviceViewer.js
@@ -1,72 +1,65 @@
 import React, { Component } from 'react'
+import styled from 'styled-components'
 import Device from './Device'
-import { StyleSheet, css } from 'aphrodisiac'
-import cx from 'classnames'
 
 class DeviceViewer extends Component {
   render() {
     const {props: {src}} = this
     return (
-      <div className={css(s.DeviceViewer)}>
-        <div className={css(s.views)}>
-          <div className={cx(css(s.view), 'laptop')}><Device device='laptop' src={src} /></div>
-          <Device className={s.view} device='tablet' src={src} landscape />
-          <Device className={s.view} device='phone' src={src} />
-        </div>
-      </div>
+      <Root>
+        <RootInner>
+          <Device2 device='laptop' src={src} />
+          <Device2 device='tablet' src={src} landscape />
+          <Device2 device='phone' src={src} />
+        </RootInner>
+      </Root>
     )
   }
 }
 
-const s = StyleSheet.create({
-  DeviceViewer: {
-    display: 'flex',
-    'align-items': 'center',
-    'justify-content': 'center',
-  },
+const Root = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
 
-  views: {
-    width: 1000,
-    height: 600,
-    'margin-left': 32,
+const RootInner = styled.div`
+  width: 1000px;
+  height: 600px;
+  margin-left: 32px;
 
-    '@media (max-width: 1024px)': {
-      views: {
-        width: 500,
-        height: 300,
-        'margin-left': '-64px',
-        'transform-origin': '0 0',
-        transform: 'scale(0.6)',
-      },
-    },
+  @media (max-width: 1024px) {
+    width: 500px;
+    height: 300px;
+    margin-left: -64px;
+    transform-origin: 0 0;
+    transform: scale(0.6);
+  }
 
-    '@media (max-width: 600px)': {
-      views: {
-        width: 250,
-        height: 150,
-        'margin-left': '-80px',
-        'transform-origin': '0 0',
-        transform: 'scale(0.35)',
-      },
-    },
-  },
+  @media (max-width: 600px) {
+    width: 250px;
+    height: 150px;
+    margin-left: -80px;
+    transform-origin: 0 0;
+    transform: scale(0.35);
+  }
+`
 
-  view: {
-    position: 'absolute',
-    'transform-origin': 'left top',
+const Device2 = styled(Device)`
+  position: absolute;
+  transform-origin: left top;
 
-    '&.laptop': {
-      transform: 'translate3d(0px, 40px, 0) scale(0.5)',
-    },
+  &.laptop {
+    transform: translate3d(0px, 40px, 0) scale(0.5);
+  }
 
-    '&.tablet': {
-      transform: 'translate3d(380px, 130px, 0) scale(0.5)',
-    },
+  &.tablet {
+    transform: translate3d(380px, 130px, 0) scale(0.5);
+  }
 
-    '&.phone': {
-      'transform': 'translate3d(800px, 110px, 0) scale(0.5)',
-    }
-  },
-})
+  &.phone {
+    transform: translate3d(800px, 110px, 0) scale(0.5);
+  }
+`
 
 export default DeviceViewer
