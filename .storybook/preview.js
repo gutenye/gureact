ebc8a064f4677f678502e0715b7617335b646e39
Guten Ye <ywzhaifei@gmail.com>
2021-01-14 20:31:12 +0800

    add table of content

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,17 @@
+import React from 'react'
+import { DocsContainer } from '@storybook/addon-docs/blocks'
+import { BackToTop, TableOfContents } from 'storybook-docs-toc'
 
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
+  viewMode: 'docs',
+  docs: {
+    container: (props) => (
+      <React.Fragment>
+        <TableOfContents />
+        <DocsContainer {...props} />
+        <BackToTop />
+      </React.Fragment>
+    ),
+  },
 }
