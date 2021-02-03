1ca050a5f62aa990dc774ab88ced298ebbdc6172
Guten Ye <ywzhaifei@gmail.com>
2021-02-03 20:46:45 +0800

    update form step

--- react-final-form/FormSteps.tsx
+++ react-final-form/FormSteps.tsx
@@ -1,21 +1,23 @@
 import React from 'react'
-import { Form, FormProps } from 'react-final-form'
+import { Form as FinalForm, FormProps } from 'react-final-form'
 
 /**
  *
  * <FormSteps
- *   page, onPageChange
- *   onSubmit
- *   Buttons: ({ isFirstPage, isLastPage, previous, submitting, ...rest }) => (
+ *   page: 0, onPageChange, onSubmit
+ *   totalPage      // will loop if totalPage > children count
+ *   Form: styled('form'),
+ *   ButtonsPosition: 'after'
+ *   Buttons: ({ isFirstPage, isLastPage, page, totalPage, previous, next, submitting, ...rest }) => (
  *     <div>
  *       {!isFirstPage && <button type="button" onClick={previous}>Previous</button>}
- *       {!isLastPage && <button type="submit">Next »</button>}
+ *       {!isLastPage && <button type="button" onClick={next}>Next</button>}
  *       {isLastPage && <button type="submit" disabled={submitting}>Submit</button>}
  *     </div>
  *   )
  * >
  *   <FormSteps.Page validate render component children />  // Page-level validation
- *     props: { values, .. }  // from react-final-form
+ *     props: { ...ButtonsProps, values, .. }  // from react-final-form
  *   ...
  * </FormSteps>
  *
@@ -27,39 +29,71 @@ interface Props extends FormProps {
   Buttons: any
   onSubmit: any
   children: any
+  totalPage?: number
 }
 
 class FormSteps extends React.Component<Props> {
   static Page: any
 
+  static defaultProps = {
+    ButtonsPosition: 'after',
+  }
+
   render() {
-    const { page, onPageChange, children, ...rest } = this.props
-    const activePage = React.Children.toArray(children)[page] as any
-    const totalPage = React.Children.toArray(children).length
-    const isLastPage = page === React.Children.count(children) - 1
+    const { Form, page, ButtonsPosition, ...rest } = this.props
+    const activePage = this.activePage()
+    const totalPage = this.totalPage()
+    const isLastPage = page === totalPage - 1
     const isFirstPage = page === 0
+    const buttons = this.props.Buttons({
+      isFirstPage,
+      isLastPage,
+      page,
+      totalPage,
+      previous: this.previous,
+      next: this.next,
+      ...rest,
+    })
     return (
-      <Form {...rest} validate={this.validate} onSubmit={this.onSubmit}>
+      <FinalForm
+        {...rest}
+        validate={this.validate}
+        onSubmit={this.props.onSubmit}
+      >
         {({ handleSubmit, ...renderRest }) => (
-          <form onSubmit={handleSubmit}>
-            {renderComponent({ ...activePage.props, ...renderRest })}
-            {this.props.Buttons({
+          <Form onSubmit={handleSubmit}>
+            {ButtonsPosition === 'before' && buttons}
+            {renderComponent({
               isFirstPage,
               isLastPage,
-              page: page + 1,
+              page,
               totalPage,
               previous: this.previous,
-              ...rest,
+              next: this.next,
+              ...activePage.props,
+              ...renderRest,
             })}
-          </form>
+            {ButtonsPosition === 'after' && buttons}
+          </Form>
         )}
-      </Form>
+      </FinalForm>
     )
   }
 
-  next = () => {
+  totalPage = () => {
+    const { totalPage, children } = this.props
+    return totalPage || React.Children.count(children)
+  }
+
+  activePage = () => {
     const { page, children } = this.props
-    const newPage = Math.min(page + 1, children.length - 1)
+    const childrenIndex = Math.min(page, React.Children.count(children) - 1)
+    return React.Children.toArray(children)[childrenIndex] as any
+  }
+
+  next = () => {
+    const { page } = this.props
+    const newPage = Math.min(page + 1, this.totalPage() - 1)
     this.props.onPageChange(newPage)
   }
 
@@ -75,20 +109,9 @@ class FormSteps extends React.Component<Props> {
    * functions once the form has been defined.
    */
 
-  validate = (values) => {
-    const { page, children } = this.props
-    const activePage = React.Children.toArray(children)[page] as any
+  validate = values => {
+    const activePage = this.activePage()
     return activePage.props.validate ? activePage.props.validate(values) : {}
-  }
-
-  onSubmit = (values) => {
-    const { page, children } = this.props
-    const isLastPage = page === React.Children.count(children) - 1
-    if (isLastPage) {
-      return this.props.onSubmit(values)
-    } else {
-      this.next()
-    }
   }
 }
 
---
+++ react-final-form/index.tsx
@@ -0,0 +1,2 @@
+export { default as FieldValue } from './FieldValue'
+export { default as FormSteps } from './FormSteps'
