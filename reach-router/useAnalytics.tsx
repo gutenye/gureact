905d362817591195bfa5fcc42e0984e205e48ee9
Guten Ye <ywzhaifei@gmail.com>
2021-02-12 22:52:06 +0800

    remove log

--- reach-router/useAnalytics.tsx
+++ reach-router/useAnalytics.tsx
@@ -7,7 +7,6 @@ GA.initialize(process.env.REACT_APP_GA_ID)
 const useAnalytics = () => {
   const pathname = useLocation().pathname
   useEffect(() => {
-    console.log(':: ~ useEffect ~ pathname', pathname)
     GA.pageview(pathname)
   }, [pathname])
 }
